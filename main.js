cfe089a63d62a2538cbd546a56270dae8c349fa8
Partenie Alex <parteniealex@yahoo.com>
2020-01-09 14:24:35 +0200

    added pieceOffset and deleted unless code

--- main.js
+++ main.js
@@ -12,13 +12,15 @@ var canvasDif;
 var ctxD;
 var pieces;
 var clickedPiece;
+var pieceOffsetX = 0;
+var pieceOffsetY = 0;
 var img;
 var mouse;
 var mouseB;
 var mouseD;
 var t = null;
 
-$(document).ready(function () {
+$(document).ready(function() {
   $("#conta").hide();
 });
 
@@ -39,16 +41,16 @@ function handleFileSelect(evt) {
 function handleDragOver(evt) {
   evt.stopPropagation();
   evt.preventDefault();
-  evt.dataTransfer.dropEffect = 'copy';
+  evt.dataTransfer.dropEffect = "copy";
 }
 
-window.addEventListener('dragover', handleDragOver, false);
-window.addEventListener('drop', handleFileSelect, false);
+window.addEventListener("dragover", handleDragOver, false);
+window.addEventListener("drop", handleFileSelect, false);
 
 window.onresize = () => {
   if (canvas) {
     if (t != null) clearTimeout(t);
-    t = setTimeout(function () {
+    t = setTimeout(function() {
       compress(file);
     }, 20);
   }
@@ -62,11 +64,11 @@ $("#uploadedFile").change((e) => {
 function compress(file) {
   var reader = new FileReader();
   reader.readAsDataURL(file);
-  reader.onload = event => {
+  reader.onload = (event) => {
     var img = new Image();
     img.src = event.target.result;
-    img.onload = () => {
-      var tempCanv = document.createElement('canvas');
+    (img.onload = () => {
+      var tempCanv = document.createElement("canvas");
 
       var height = window.innerHeight * 0.8;
       var scaleTemp = height / img.height;
@@ -74,16 +76,16 @@ function compress(file) {
       tempCanv.height = height;
       tempCanv.width = img.width * scaleTemp;
 
-      var context = tempCanv.getContext('2d');
+      var context = tempCanv.getContext("2d");
       context.drawImage(img, 0, 0, tempCanv.width, tempCanv.height);
 
-      var data = context.canvas.toDataURL(img, 'image/jpeg', 1);
+      var data = context.canvas.toDataURL(img, "image/jpeg", 1);
       var result = new Image();
       result.src = data;
 
       startGame(result);
-    },
-      reader.onerror = error => console.log(error);
+    }),
+      (reader.onerror = (error) => console.log(error));
   };
 }
 
@@ -127,8 +129,18 @@ function configCanvas() {
   ctxB.stroke();
   ctxB.font = "20px Verdana";
   ctxB.fillStyle = "black";
-  ctxB.fillText("Randomize", 0, canvasButtons.height / 2 + 7, canvasButtons.width / 2);
-  ctxB.fillText("Solve", canvasButtons.width / 2, canvasButtons.height / 2 + 7, canvasButtons.width / 2);
+  ctxB.fillText(
+    "Randomize",
+    0,
+    canvasButtons.height / 2 + 7,
+    canvasButtons.width / 2,
+  );
+  ctxB.fillText(
+    "Solve",
+    canvasButtons.width / 2,
+    canvasButtons.height / 2 + 7,
+    canvasButtons.width / 2,
+  );
 
   canvasDif = document.getElementById("canvasDif");
   ctxD = canvasDif.getContext("2d");
@@ -142,20 +154,25 @@ function configCanvas() {
 
 function difficultyEvent(e) {
   mouseD = mouseUpdate(e, canvasDif);
-  difficulty = Math.round(mouseD.x / canvasDif.width * 4) + 2;
+  difficulty = Math.round((mouseD.x / canvasDif.width) * 4) + 2;
   drawDif();
   console.log(difficulty);
   onImage();
 }
 
 function drawDif() {
-  var fill = (difficulty - 2) / 4 * canvasDif.width;
+  var fill = ((difficulty - 2) / 4) * canvasDif.width;
   ctxD.fillStyle = "white";
   ctxD.fillRect(0, 0, canvasDif.width, canvasDif.height);
   ctxD.fillStyle = "cyan";
   ctxD.fillRect(0, 0, fill, canvasDif.height);
   ctxD.fillStyle = "black";
-  ctxD.fillText("Dificulty: " + difficulty, 0, canvasDif.height / 2 + 4, canvasDif.width);
+  ctxD.fillText(
+    "Dificulty: " + difficulty,
+    0,
+    canvasDif.height / 2 + 4,
+    canvasDif.width,
+  );
 }
 
 function setSolveButton() {
@@ -180,6 +197,7 @@ function eventSolve(e) {
     solve();
   }
 }
+
 function eventShuffle(e) {
   mouseB = mouseUpdate(e, canvasButtons);
   if (mouseB.x < canvasButtons.width / 2) {
@@ -190,9 +208,22 @@ function eventShuffle(e) {
 function initPuzzle() {
   disableClick();
   pieces = [];
-  mouse = { x: 0, y: 0 };
+  mouse = {
+    x: 0,
+    y: 0,
+  };
   clickedPiece = null;
-  ctx.drawImage(img, 0, 0, puzzleWidth, puzzleHeight, 0, 0, puzzleWidth, puzzleHeight);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    puzzleWidth,
+    puzzleHeight,
+    0,
+    0,
+    puzzleWidth,
+    puzzleHeight,
+  );
   build();
 }
 
@@ -200,10 +231,7 @@ function build() {
   var calcX = 0;
   var calcY = 0;
   for (let i = 0; i < difficulty * difficulty; i++) {
-    var piece = {};
-    piece.index = i;
-    piece.solX = calcX;
-    piece.solY = calcY;
+    var piece = { key: i, solX: calcX, solY: calcY, curX: 0, curY: 0 };
     pieces.push(piece);
     calcX += pieceWidth;
     if (calcX >= puzzleWidth) {
@@ -221,7 +249,17 @@ function shuffle() {
     var randomY = Math.floor(Math.random() * (puzzleHeight - pieceHeight));
     pieces[i].curX = randomX;
     pieces[i].curY = randomY;
-    ctx.drawImage(img, pieces[i].solX, pieces[i].solY, pieceWidth, pieceHeight, randomX, randomY, pieceWidth, pieceHeight);
+    ctx.drawImage(
+      img,
+      pieces[i].solX,
+      pieces[i].solY,
+      pieceWidth,
+      pieceHeight,
+      randomX,
+      randomY,
+      pieceWidth,
+      pieceHeight,
+    );
   }
   setSolveButton();
   document.onmousedown = onPuzzleClick;
@@ -229,9 +267,14 @@ function shuffle() {
 
 function getPiece() {
   for (let i = 0; i < pieces.length; i++) {
-    if (mouse.x < pieces[i].curX || mouse.x > (pieces[i].curX + pieceWidth) || mouse.y < pieces[i].curY || mouse.y > (pieces[i].curY + pieceHeight)) {
-    }
-    else {
+    if (
+      !(
+        mouse.x < pieces[i].curX ||
+        mouse.x > pieces[i].curX + pieceWidth ||
+        mouse.y < pieces[i].curY ||
+        mouse.y > pieces[i].curY + pieceHeight
+      )
+    ) {
       return pieces[i];
     }
   }
@@ -243,13 +286,12 @@ function onPuzzleClick(e) {
   clickedPiece = getPiece();
 
   if (clickedPiece != null) {
-    new Audio('./media/hold.wav').play();
-    ctx.clearRect(clickedPiece.curX, clickedPiece.curY, pieceWidth, pieceHeight);
+    pieces.splice(pieces.indexOf(clickedPiece), 1);
+    pieces.push(clickedPiece);
+    pieceOffsetX = mouse.x - clickedPiece.curX;
+    pieceOffsetY = mouse.y - clickedPiece.curY;
+    new Audio("./media/hold.wav").play();
     updatePuzzle(e);
-    ctx.save();
-    ctx.globalAlpha = .7;
-    ctx.drawImage(img, clickedPiece.solX, clickedPiece.solY, pieceWidth, pieceHeight, mouse.x - (pieceWidth / 2), mouse.y - (pieceHeight / 2), pieceWidth, pieceHeight);
-    ctx.restore();
     document.onmousemove = updatePuzzle;
     document.onmouseup = dropPiece;
   }
@@ -259,23 +301,42 @@ function updatePuzzle(e) {
   mouse = mouseUpdate(e, canvas);
   ctx.clearRect(0, 0, puzzleWidth, puzzleHeight);
   for (let i = 0; i < pieces.length; i++) {
-    if (pieces[i] == clickedPiece) {
-      continue;
+    if (pieces[i] != clickedPiece) {
+      ctx.drawImage(
+        img,
+        pieces[i].solX,
+        pieces[i].solY,
+        pieceWidth,
+        pieceHeight,
+        pieces[i].curX,
+        pieces[i].curY,
+        pieceWidth,
+        pieceHeight,
+      );
     }
-    ctx.drawImage(img, pieces[i].solX, pieces[i].solY, pieceWidth, pieceHeight, pieces[i].curX, pieces[i].curY, pieceWidth, pieceHeight);
   }
   ctx.save();
-  ctx.globalAlpha = .7;
-  ctx.drawImage(img, clickedPiece.solX, clickedPiece.solY, pieceWidth, pieceHeight, mouse.x - (pieceWidth / 2), mouse.y - (pieceHeight / 2), pieceWidth, pieceHeight);
+  ctx.globalAlpha = 0.7;
+  ctx.drawImage(
+    img,
+    clickedPiece.solX,
+    clickedPiece.solY,
+    pieceWidth,
+    pieceHeight,
+    mouse.x - pieceOffsetX,
+    mouse.y - pieceOffsetY,
+    pieceWidth,
+    pieceHeight,
+  );
   ctx.restore();
 }
 
-function dropPiece(e) {
-  clickedPiece.curX = mouse.x - (pieceWidth / 2);
-  clickedPiece.curY = mouse.y - (pieceHeight / 2);
+function dropPiece() {
+  clickedPiece.curX = mouse.x - pieceOffsetX;
+  clickedPiece.curY = mouse.y - pieceOffsetY;
   document.onmousemove = null;
   document.onmouseup = null;
-  new Audio('./media/release.wav').play();
+  new Audio("./media/release.wav").play();
   checkAndReset();
 }
 
@@ -313,9 +374,19 @@ function solve() {
         piece = pieces[i];
         piece.curX = piece.curX + (piece.solX - piece.curX) * progress;
         piece.curY = piece.curY + (piece.solY - piece.curY) * progress;
-        ctx.drawImage(img, piece.solX, piece.solY, pieceWidth, pieceHeight, piece.curX, piece.curY, pieceWidth, pieceHeight);
+        ctx.drawImage(
+          img,
+          piece.solX,
+          piece.solY,
+          pieceWidth,
+          pieceHeight,
+          piece.curX,
+          piece.curY,
+          pieceWidth,
+          pieceHeight,
+        );
       }
-    }
+    },
   });
 }
 
@@ -328,9 +399,25 @@ function checkAndReset() {
   var error = Math.min(puzzleWidth / 20, puzzleHeight / 20);
   var win = true;
   for (let i = 0; i < pieces.length; i++) {
-    ctx.drawImage(img, pieces[i].solX, pieces[i].solY, pieceWidth, pieceHeight, pieces[i].curX, pieces[i].curY, pieceWidth, pieceHeight);
-    if (!between(pieces[i].curX, pieces[i].solX - error, pieces[i].solX + error) ||
-      !between(pieces[i].curY, pieces[i].solY - error, pieces[i].solY + error)) {
+    ctx.drawImage(
+      img,
+      pieces[i].solX,
+      pieces[i].solY,
+      pieceWidth,
+      pieceHeight,
+      pieces[i].curX,
+      pieces[i].curY,
+      pieceWidth,
+      pieceHeight,
+    );
+    if (
+      !between(
+        pieces[i].curX,
+        pieces[i].solX - error,
+        pieces[i].solX + error,
+      ) ||
+      !between(pieces[i].curY, pieces[i].solY - error, pieces[i].solY + error)
+    ) {
       win = false;
     }
   }
@@ -340,7 +427,7 @@ function checkAndReset() {
 }
 
 function gameOver() {
-  var done = new Audio('./media/done.wav');
+  var done = new Audio("./media/done.wav");
   done.volume = 0.5;
   done.play();
   initPuzzle();
